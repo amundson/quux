41dfc3e04d3ca0af49a72316b789874d6c3590cd
James Amundson <amundson@fnal.gov>
2016-01-11 17:04:13 -0600

    add garply dependency

--- quux/quux.cc
+++ quux/quux.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "quux.h"
 #include "quux_version.h"
+#include "garply/garply.h"
 
 const int Quux::version_major = quux_version_major;
 const int Quux::version_minor = quux_version_minor;
@@ -18,7 +19,16 @@ Quux::get_version() const
 int
 Quux::quuxifier() const
 {
-    std::cout << "Quux::quuxifier version " << get_version()
+    int quux_version = get_version();
+    std::cout << "Quux::quuxifier version " << quux_version
               << " invoked\n";
-    return get_version();
+
+    Garply garply;
+    int garply_version = garply.garplinator();
+
+    if(garply_version != quux_version) {
+        throw std::runtime_error("Quux found an incompatible version of Garply.");
+    }
+
+    return quux_version;
 }
